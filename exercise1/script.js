1c0f1f4ee4761eb2206848fce1f66b23fc84797f
iqrahussein <iqramohamed600@gmail.com>
2026-01-28 16:24:01 +0300

    js exercise 1

---
+++ exercise1/script.js
@@ -0,0 +1,15 @@
+let x=7;
+let y=3;
+
+
+console.log("the subtract of x and y is ",x+y);
+
+console.log("the sum of x and y is ",x-y);
+
+console.log("the multiplay of x and y is ",x*y);
+
+console.log("the division of x and y is ",x/y);
+
+console.log( "the module of x and y is ",x % y);  
+console.log("the increment of x",x++);    
+console.log("the decremant of x " ,x-- );    
