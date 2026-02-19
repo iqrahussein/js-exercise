fc98b90c6cec74e699604837a64dcda3f2bf044d
iqrahussein <iqramohamed600@gmail.com>
2026-02-19 12:10:57 +0300

    exercise 4

---
+++ exercise4/function.js
@@ -0,0 +1,15 @@
+function add (a,b){
+  console.log(a+b);
+}
+
+add(2,2)
+add(21,12)
+add(12,211)
+add(22,20)
+
+const addexp=function (a,b){
+  return a+b
+}
+
+let result=addexp(10,10)
+console.log(result*2)
