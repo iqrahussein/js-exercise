587e1afe40a9cde313a58d2e6feeaba0b7d83e71
iqrahussein <iqramohamed600@gmail.com>
2026-02-20 12:33:50 +0300

    js exercise6

---
+++ exercise6/arrays.js
@@ -0,0 +1,9 @@
+let colors=["pink","purble","blue","black","white"];
+
+console.log(colors);
+
+console.log(colors[0])
+console.log(colors[1])
+console.log(colors[2])
+console.log(colors[3])
+console.log(colors[4])
