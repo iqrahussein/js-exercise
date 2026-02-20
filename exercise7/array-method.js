2546fbedb812b059741080d353131287df28a019
iqrahussein <iqramohamed600@gmail.com>
2026-02-20 13:34:04 +0300

    js exercise7

---
+++ exercise7/array-method.js
@@ -0,0 +1,22 @@
+let colors=["yellow","green","orange","black"]
+console.log(colors)
+
+colors.push("blue");
+console.log("After push")
+console.log(colors)
+
+
+colors.pop();
+console.log("after pop")
+console.log(colors)
+
+colors.unshift("purble");
+console.log("after unshift")
+console.log(colors)
+
+colors.shift();
+console.log("after shift")
+console.log(colors)
+
+//length 
+console.log("the number of the array is "+colors.length);
