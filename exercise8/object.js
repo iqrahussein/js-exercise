82201dddd2d0a41da430caedc0bd68f87d049e34
iqrahussein <iqramohamed600@gmail.com>
2026-02-20 13:59:02 +0300

    js exercise8

---
+++ exercise8/object.js
@@ -0,0 +1,14 @@
+let car ={
+
+  make:"toyoto",
+  model:"carola",
+  year:2021
+}
+
+car.year=2022
+car.number="carola8"
+
+console.log(car.make)
+console.log(car.model)
+console.log(car.year)
+console.log(car.number)
