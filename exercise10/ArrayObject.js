aae74b75ef76929b3f8fa95827ac149025540e72
iqrahussein <iqramohamed600@gmail.com>
2026-02-21 16:59:15 +0300

    js exercise 10

---
+++ exercise10/ArrayObject.js
@@ -0,0 +1,29 @@
+let library=[
+  {
+    title : "atomic habit",
+    author : "antony",
+    year :2020
+  },
+
+  {
+    title : "dhanbaalka qur'aanka",
+    author : "qays bin saacid",
+    year :2020
+  },
+
+  {
+    title : "Soo doog",
+    author : "abdulqaadir diiriye",
+    year :2022
+  },
+
+  {
+    title : "psychology of money",
+    author : "Morgan",
+    year :2019
+  },
+]
+
+console.log(library)
+console.log("the title of frist book is "+library[0].title)
+console.log("the auther of second book is "+library[1].author)
