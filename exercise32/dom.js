dcbfe11078abc93b1ea5148c9c369fd285949b47
iqrahussein <iqramohamed600@gmail.com>
2026-03-21 18:09:55 +0300

    js exercise32 DOM

---
+++ exercise32/dom.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('#header');
+console.log(header)
+
+const text =document.querySelectorAll('.text');
+
+console.log(text)
+
+const buttons=document.querySelectorAll('button');
+console.log(buttons)
